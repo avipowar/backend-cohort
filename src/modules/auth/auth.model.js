33fab241890c22d5975cbd8b79755d2953ee9d92
Avinash Powar <powaravinash06@gmail.com>
2026-04-09 13:51:26 +0530

    add some mongodb document in schema

--- src/modules/auth/auth.model.js
+++ src/modules/auth/auth.model.js
@@ -12,8 +12,43 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: true,
-        required : [true, "email is required"]
+        lowercase: true,
+        required : [true, "Email is required"]
+    },
+    password : {
+        type: String,
+        trim: true,
+        required : [true, "Password is required"],
+        minlength : 8,
+        maxlength : 28,
+        select: false
+    },
+    role : {
+        type : String,
+        enum : ["customer", "seller", "admin"],
+        default : "customer"
+    },
+    isVerified: {
+        type : Boolean,
+        default : false
+    },
+    verificationToken: {
+        type : String,
+        select: false
+    },
+    refreshToken: {
+        type : String,
+        select: false
+    },
+    resetPasswordToken: {
+        type : String,
+        select: false
+    },
+    resetPasswordExpires: {
+        type : Date,
+        select: false
     }
-})
+    
+}, {timestamps: true})
 
 export default mongoose.model("User", userSchema)
