109b4ae0f5619f5c72f5971bdf51a7351cf85efe
Avinash Powar <powaravinash06@gmail.com>
2026-04-14 20:36:20 +0530

    write dto fro new password

--- src/modules/auth/dto/newPassword.dto.js
+++ src/modules/auth/dto/newPassword.dto.js
@@ -2,7 +2,15 @@ import BaseDto from "../../../common/dto/base.dto.JS"
 import Joi from "joi"
 
 class NewPasswordDto extends BaseDto {
+    static schema = Joi.object({
+        token : Joi.string().required().message({
+            "any.required ": "Token is required"
+        }),
 
+        password: Joi.string().min(8).required().message({
+            "string.min" :  "Password must be at least 8 characters",
+        })
+    }) 
 }
 
-export default NewPasswordDto
+export default NewPasswordDto;
